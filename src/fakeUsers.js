429d56e2f18f51e4022e13b749f427fa77da8626
daniel-monsalve-villegas <unbigote@hush.com>
2022-10-25 07:11:17 -0400

    try/catch añadido al generador de nombres aleatorios

--- src/fakeUsers.js
+++ src/fakeUsers.js
@@ -2,19 +2,18 @@ const faker = require ('faker/locale/es');
 const fs = require ('fs');
 
 function generateUser() {
-	try {
 	let users = [];
-	for(let id = 1; id <= 1000; id++) {
-		const firstName = faker.name.firstName();
-		const lastName = faker.name.lastName();
+	try {
+		for(let id = 1; id <= 1000; id++) {
+			const firstName = faker.name.firstName();
+			const lastName = faker.name.lastName();
 
-		users.push({
-			id: id,
-			firstName: firstName,
-			lastName: lastName
-		});
-	}
-
+			users.push({
+				id: id,
+				firstName: firstName,
+				lastName: lastName
+			});
+		}
 		return { data: users }
 	} catch (err) {
 		console.log(err);
